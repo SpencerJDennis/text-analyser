2f619f38e24101d102f9fbb1808b926cdda152f0
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-16 14:04:11 -0700

    error in commit fixing

--- js/scripts.js
+++ js/scripts.js
@@ -38,8 +38,18 @@ function removeOffensiveWords(text) {
   return wordCount;
 }
 
+//UI Logic
 
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
 
-
-
-
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
